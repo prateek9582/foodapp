6dc8645a600773f0bf53dc3f7abfbf46d1e44611
Prateek <gprateek174@gmail.com>
2020-02-09 19:46:15 +0530

    updated a file

--- routers/bookingRouter.js
+++ routers/bookingRouter.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bookingRouter = express.Router();
 
 const {createCheckoutSession, createNewBooking} = require("../controllers/bookingController");
-const {isuserverified} = require("../controllers/authController");
-bookingRouter.use(isuserverified);
+// const {isuserverified} = require("../controllers/authController");
+// bookingRouter.use(isuserverified);
 bookingRouter.get("/:id",createCheckoutSession);
 bookingRouter.post("/createNewBooking",createNewBooking);
 
